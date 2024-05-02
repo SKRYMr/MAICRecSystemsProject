74ad225c1e62be5ce6e62b960a04d1fdea208297
SKRYMr <alessio.boiardi@fastwebnet.it>
2024-05-02 12:18:35 +0200

    Finished setting up models and views, flushed database, removed data from git

--- Recommender/models.py
+++ Recommender/models.py
@@ -5,12 +5,19 @@ from django.db import models
 
 class Movie(models.Model):
     movie_id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=100)
-    director = models.CharField(max_length=100)
-    year = models.IntegerField()
-    actors = models.JSONField(default=list)     # Needs to be a function that returns a fresh object each time
-    synopsis = models.TextField(max_length=5000)
-    genres = models.JSONField(default=list)
+    title = models.CharField(max_length=500)
+    director = models.CharField(max_length=100, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    actors = models.TextField(null=True, blank=True)
+    synopsis = models.TextField(max_length=5000, null=True, blank=True)
+    genres = models.TextField()
+    poster = models.CharField(max_length=100, null=True, blank=True)
+
+    def get_base_url(self):
+        """
+        Returns the base url for the movie posters, just append whatever is in the "poster" field.
+        """
+        return "https://image.tmdb.org/t/p/original"
 
 
 class User(models.Model):
@@ -25,6 +32,6 @@ class Rating(models.Model):
         4: "Good",
         5: "Very Good"
     }
-    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(choices=RATINGS)
--- Recommender/views.py
+++ Recommender/views.py
@@ -1,6 +1,4 @@
-import pickle
-import os
-
+import pandas as pd
 from django.http import Http404
 from django_pandas.io import read_frame
 from django.shortcuts import render
@@ -16,15 +14,14 @@ def index(request):
 def recommend(request):
     users = set(User.objects.values_list("user_id", flat=True))
     movies = read_frame(Movie.objects.all())
-    ratings = read_frame(Rating.objects.all())
+    ratings = pd.DataFrame(list(Rating.objects.values_list("movie_id", "user_id", "rating")),
+                           columns=["movie_id", "user_id", "rating"])
     try:
         user_predictions = get_movies_recommendations(request.POST["user_id"], users, ratings, movies,
                                                       minimum_ratings=3,
                                                       metric="neighbours_average")
     except FileNotFoundError as e:  # TODO: Replace with the correct error
         raise Http404("User does not exist")
-    print(user_predictions)
     context = {"recommendations": list(user_predictions["title"])}
-    print(context)
     return render(request, "recommendations.html", context)
 
